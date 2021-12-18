84d5ba469594ef1e91295731b5e5ef5a0b297696
Rumon <alshariar777@gmail.com>
2021-12-18 09:54:19 +0600

    https://github.com/RUMON77/es6-practice

---
+++ threedots.js
@@ -0,0 +1,12 @@
+const ages = [12, 14, 16, 13, 17];
+const ages2 = [15, 16, 12];
+
+const allAges = [...ages, ...ages2,];
+// console.log(allAges);
+
+const taka = [20, 30, 40, 70
+
+];
+const maximum = Math.max(...taka);
+console.log(maximum);
+
